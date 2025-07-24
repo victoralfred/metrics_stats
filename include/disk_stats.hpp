980eca37722691082f16261df1af04f7c9d6cc1f
Victor Oseghale <voseghale1@gmail.cm>
2025-07-24 07:20:05 +0200

    Base sytem performance information collected

--- include/disk_stats.hpp
+++ include/disk_stats.hpp
@@ -1,7 +1,26 @@
+#pragma once
+#ifndef SYSTEM_DISK_STATS_H
+#define SYSTEM_DISK_STATS_H
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 namespace SysytemDiskStats {
 
 struct DiskStats {
     unsigned long long read_bytes, write_bytes;
     double read_rate, write_rate; // bytes/sec
 };
+class DiskStatsReader {
+public:
+    static DiskStats getDiskStats();
+    static DiskStats getDiskStats(std::string& device);
+    DiskStatsReader() = delete;
+    ~DiskStatsReader() = delete;
+    DiskStatsReader(const DiskStatsReader&) = delete;
+    DiskStatsReader& operator=(const DiskStatsReader&) = delete;
+};
 }
+#endif // SYSTEM_DISK_STATS_H
